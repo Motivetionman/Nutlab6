880c3a805c48db570eeaa554b353ac102e8e2983
Motivationman_74 <120492095+Motivetionman@users.noreply.github.com>
2023-12-04 02:48:49 +0700

    lab6_1.cpp completed

---
+++ lab6_1.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int x;
+    cout << "Enter an integer: ";
+    cin >> x;
+    int even = 0, odd = 0;
+
+    while (x != 0) {
+        if (x % 2 == 0) {
+            even += 1;
+        } else {
+            odd += 1;
+        }
+        cout << "Enter an integer: ";
+        cin >> x;
+    }
+
+    cout << "#Even numbers = " << even << endl;
+    cout << "#Odd numbers = " << odd << endl;
+    return 0;
+}
